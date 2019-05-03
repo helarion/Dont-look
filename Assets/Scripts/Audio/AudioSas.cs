302d7b0d3b77961b0ce62e9c010504c05e8f8e16
unknown <flo.alazais@gmail.com>
2019-05-03 13:49:11 +0200

    [UPDATED] Fix audio sas rtpc value n'allait pas jusqu'à la valeur max

--- Assets/Scripts/Audio/AudioSas.cs
+++ Assets/Scripts/Audio/AudioSas.cs
@@ -7,11 +7,17 @@ public class AudioSas : MonoBehaviour
     public enum Direction { Left, Right, Down, Up };
     [SerializeField] public Direction direction;
     bool occupied;
+    bool exited;
+    bool exitRtpcValue;
+    Collider collider;
 
     // Start is called before the first frame update
     void Start()
     {
         occupied = false;
+        exited = false;
+        exitRtpcValue = false;
+        collider = gameObject.GetComponent<Collider>();
     }
 
     // Update is called once per frame
@@ -32,17 +38,53 @@ public class AudioSas : MonoBehaviour
     {
         if (other.gameObject.name == "Player")
         {
+            Vector3 playerPosition = other.transform.position;
             occupied = false;
+            exited = true;
+            if (direction == Direction.Down)
+            {
+                exitRtpcValue = playerPosition.y < collider.bounds.min.y;
+            }
+            else if (direction == Direction.Up)
+            {
+                exitRtpcValue = playerPosition.y > collider.bounds.max.y;
+            }
+            else if (direction == Direction.Left)
+            {
+                exitRtpcValue = playerPosition.x > collider.bounds.max.x;
+            }
+            else if (direction == Direction.Right)
+            {
+                exitRtpcValue = playerPosition.x < collider.bounds.min.x;
+            }
         }
     }
 
     public Collider getCollider()
     {
-        return gameObject.GetComponent<Collider>();
+        return collider;
     }
 
     public bool getIsOccupied()
     {
         return occupied;
+    }
+
+    public bool getIsExited()
+    {
+        if (exited)
+        {
+            exited = false;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool getExitRtpcValue()
+    {
+        return exitRtpcValue;
     }
 }
